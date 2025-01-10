7e1fa42c41e8e3f32839ca81876fd623c7581c51
vladAstahov <asl.digital@mail.ru>
2025-01-10 17:56:01 +0300

    fix bug on EventCreateAddress

--- src/pages/Event/EventCreate/EventCreateAddress/EventCreateAddress.tsx
+++ src/pages/Event/EventCreate/EventCreateAddress/EventCreateAddress.tsx
@@ -46,7 +46,6 @@ export const EventCreateAddress = () => {
                 <button
                     className={styles.button}
                     onClick={() => {
-                        goBack()
                         dispatch(searchAddressModel.actions.reset())
                     }}
                 >
